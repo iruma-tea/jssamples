4f18d362161fe669a9d39f5167f80e0d95d87310
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-07-20 17:27:42 +0900

    リスト4-10まで学習完了

---
+++ chap04/exponentiationOperations.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let num1 = 2;
+let num2 = 3;
+
+let ans = num1 ** num2;
+console.log(ans);
---
+++ chap04/remainderOperation.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let num1 = 111;
+let num2 = 10;
+
+let ans = num1 % num2;
+console.log(ans);
