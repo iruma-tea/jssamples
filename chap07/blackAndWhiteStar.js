a4b7bea3d1a9938a54c72538e3ddb6f0ffa258e1
iruma-tea <shusuke.think.pad@gmail.com>
2024-07-21 17:50:37 +0900

    chap07の学習完了

---
+++ chap07/blackAndWhiteStar.js
@@ -0,0 +1,11 @@
+"use struct";
+
+let str = "";
+for (let count = 1; count <= 11; count++) {
+    if (count % 2 === 0) {
+        str += "☆\n";
+    } else {
+        str += "★\n";
+    }
+}
+console.log(str);
---
+++ chap07/continue.js
@@ -0,0 +1,13 @@
+"use struct";
+
+let continuePoint = Math.round(Math.random() * 10);
+console.log("continuePointの値: " + continuePoint);
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i + "番目のループ開始");
+    if (i === continuePoint) {
+        console.log("continuePointなのでcontinue");
+        continue;
+    }
+    console.log(i + "番目のループ終了");
+}
