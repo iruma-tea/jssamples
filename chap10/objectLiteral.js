95d03d124295690362cd6bfe298ea16755328f80
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-07-27 16:55:30 +0900

    chap10の学習開始
    リスト10-5まで学習完了

---
+++ chap10/objectLiteral.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let studentNumList = { A: 30, B: 31, C: 29, D: 30, E: 32 };
+console.log("C組の人数: " + studentNumList["C"]);
+console.log("E組の人数: " + studentNumList.E);
+
+studentNumList.F = 28;
+console.log("F組の人数: " + studentNumList.F);
