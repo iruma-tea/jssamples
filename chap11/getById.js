17dfea58d44c331cd14094b50878a66007c301af
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-07-28 18:40:03 +0900

    chap11の学習完了

---
+++ chap11/getById.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function onH1ButtonClick() {
+  let headTitle = document.getElementById("headTitle");
+
+  console.log(headTitle);
+}
+
+function onH1TextButtonClick() {
+  let headTitle = document.getElementById("headTitle");
+  let headTitleText = headTitle.textContent;
+  console.log(headTitleText);
+}
+
+function onH1TextAltButtonClick() {
+  let headTitle = document.getElementById("headTitle");
+  headTitle.textContent = "idによる取得サンプル";
+}
